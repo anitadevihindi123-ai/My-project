e981dce57cc97b11956288a2b617735215c5b590
anitadevihindi123-ai <anitadevihindi123@gmail.com>
2026-08-22 06:08:09 +0530

    Update vulkan_header_fix.h

--- ncnn/armeabi-v7a/include/ncnn/vulkan_header_fix.h
+++ ncnn/armeabi-v7a/include/ncnn/vulkan_header_fix.h
@@ -6,15 +6,28 @@
 
 #include <vulkan/vulkan_core.h>
 
-// NDK r26 compatibility mapping for NCNN GPU features
+// --- NCNN to NDK r26 KHR/NV Compatibility Aliases ---
 #if defined(VK_VERSION_1_0)
+
+// 1. Cooperative Matrix Mappings
 typedef VkPhysicalDeviceCooperativeMatrixFeaturesNV VkPhysicalDeviceCooperativeMatrixFeaturesKHR;
 typedef VkPhysicalDeviceCooperativeMatrixPropertiesNV VkPhysicalDeviceCooperativeMatrixPropertiesKHR;
 typedef VkCooperativeMatrixPropertiesNV VkCooperativeMatrixPropertiesKHR;
+typedef VkComponentTypeNV VkComponentTypeKHR;
+typedef VkScopeNV VkScopeKHR;
 
 #ifndef PFN_vkGetPhysicalDeviceCooperativeMatrixPropertiesKHR
 typedef PFN_vkGetPhysicalDeviceCooperativeMatrixPropertiesNV PFN_vkGetPhysicalDeviceCooperativeMatrixPropertiesKHR;
 #endif
-#endif
+
+// 2. Robustness2 Mappings
+typedef VkPhysicalDeviceRobustness2FeaturesEXT VkPhysicalDeviceRobustness2FeaturesKHR;
+typedef VkPhysicalDeviceRobustness2PropertiesEXT VkPhysicalDeviceRobustness2PropertiesKHR;
+
+// 3. Shader & Additional Feature Mappings
+typedef VkPhysicalDeviceShaderFloat16Int8Features VkPhysicalDeviceShaderFloatControls2FeaturesKHR;
+typedef VkPhysicalDeviceShaderSubgroupExtendedTypesFeatures VkPhysicalDeviceShaderSubgroupRotateFeaturesKHR;
+
+#endif // VK_VERSION_1_0
 
 #endif // NCNN_VULKAN_HEADER_FIX_H
