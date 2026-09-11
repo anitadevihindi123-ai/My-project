fb1678125427642458b1888c0310c2517135ab58
anitadevihindi123-ai <anitadevihindi123@gmail.com>
2026-09-11 10:33:50 +0530

    Update true-singularity-core.cpp

--- app/src/main/cpp/true-singularity-core.cpp
+++ app/src/main/cpp/true-singularity-core.cpp
@@ -271,7 +271,7 @@ VK_CHECK(vkBindImageMemory(device, newImg.vkImage, newImg.vkMemory, 0));
             }
         }
         return VK_NULL_HANDLE;
-    }
+    
 
     ~PureMetalEngine() {
        thermalRunning = false;
