ebd6028e7e27ffb1df73b6c0b5ac29d99c3cd263
anitadevihindi123-ai <anitadevihindi123@gmail.com>
2026-09-12 05:55:20 +0530

    Update EngineCoreCompilerWrapper.cpp

--- app/src/main/cpp/EngineCoreCompilerWrapper.cpp
+++ app/src/main/cpp/EngineCoreCompilerWrapper.cpp
@@ -4,11 +4,10 @@
 #include <string>
 #include <filesystem>
 #include <cstdlib>
-
 #include <clang/Driver/Driver.h>
 #include <clang/Frontend/CompilerInstance.h>
 #include <clang/Tooling/Tooling.h>
-#include <llvm/Support/Host.h>
+#include <llvm/TargetParser/Host.h>
 #include <clang/AST/ASTConsumer.h>
 #include <clang/AST/RecursiveASTVisitor.h>
 #include <clang/Frontend/FrontendAction.h>
